ce424fd84dda9ae5b5023c2d9731641073e84b20
chaixb <1481569358@qq.com>
2019-03-30 17:57:32 +0800

    finish

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
+	"github.com/vmihailenco/msgpack"
 	"time"
 )
 
@@ -30,7 +31,20 @@ func main() {
 
 	// routes
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
+		key := GetStoreKey()
+		stringifiedPersons := client.SMembers(key).Val()
+		var persons []Person
+		for _, person := range stringifiedPersons {
+			var p Person
+			err := msgpack.Unmarshal([]byte(person), &p)
+			if err == nil {
+				persons = append(persons, p)
+			}
+		}
+
+		c.HTML(200, "index.html", gin.H{
+			"persons": persons,
+		})
 	})
 
 	r.GET("/members/list", func(c *gin.Context) {
@@ -38,15 +52,11 @@ func main() {
 	})
 
 	r.POST("/toggle", func(c *gin.Context) {
-		// 获取当前的日期
-		now := time.Now()
-
-		// 使用当前日期作为redis key
-		key := fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
-
 		var person Person
+		key := GetStoreKey()
 		if c.ShouldBind(&person) == nil {
-			client.SAdd(key, person.Name).Result()
+			stringifiedPerson, _ := msgpack.Marshal(&person)
+			client.SAdd(key, stringifiedPerson)
 		}
 
 		c.JSON(200, gin.H{
@@ -56,4 +66,15 @@ func main() {
 	})
 
 	r.Run(":8080")
+}
+
+// GetStoreKey
+func GetStoreKey() string {
+	// 获取当前的日期
+	now := time.Now()
+
+	// 使用当前日期作为redis key
+	key := fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
+
+	return key
 }
